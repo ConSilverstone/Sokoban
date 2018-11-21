10e470c64b316f1f680e0772e3c351eb9ebb16a2
ConSilverstone <ConnorJamesGallagher@outlook.com>
2018-11-21 14:23:05 +0000

    Deleted unused code for now, added the .dll files

---
+++ Level.h
@@ -0,0 +1,22 @@
+#pragma once
+
+//Project Includes
+#include <SFML/Graphics.hpp>
+
+class Level
+{
+public:
+
+	Level();
+
+	void Draw(sf::RenderTarget& _target);
+	void Update(sf::Time _frameTime);
+
+	void LoadLevel(int _levelToLoad);
+	void ReloadLevel();
+	void LoadNextLevel();
+
+private:
+
+	int m_currentLevel;
+};
